74456c1708ad96b297144d580d0b380ffeb66804
Wellnaidy Adrian <wellnaidyadrian@Wellnaidys-MacBook-Air.local>
2021-06-30 21:06:40 +0700

    updates directory component state to use redux

--- src/components/directory-menu/DirectoryMenu.component.js
+++ src/components/directory-menu/DirectoryMenu.component.js
@@ -1,61 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import "./directory-menu.styles.scss";
+import { createStructuredSelector } from "reselect";
+
 import MenuItem from "../menu-item/MenuItem.component";
-import './directory-menu.styles.scss';
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
-class DirectoryMenu extends React.Component {
-  constructor() {
-    super();
+const DirectoryMenu = ({ sections }) => (
+  <div className="directory-menu">
+    {sections.map(({ id, ...otherSectionsProps }) => (
+      <MenuItem key={id} {...otherSectionsProps} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      sections: [
-        {
-          title: "topi",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          id: 1,
-          linkUrl: "shop/topi",
-        },
-        {
-          title: "jaket",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          id: 2,
-          linkUrl: "shop/jackets",
-        },
-        {
-          title: "sepatu",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          id: 3,
-          linkUrl: "shop/sneakers",
-        },
-        {
-          title: "wanita",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-          size: "large",
-          id: 4,
-          linkUrl: "shop/womens",
-        },
-        {
-          title: "pria",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-          size: "large",
-          id: 5,
-          linkUrl: "shop/mens",
-        },
-      ],
-    };
-  }
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
 
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(({ id, ...otherSectionsProps }) => (
-          <MenuItem
-            key={id}
-            {...otherSectionsProps}
-          />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default DirectoryMenu;
+export default connect(mapStateToProps)(DirectoryMenu);
