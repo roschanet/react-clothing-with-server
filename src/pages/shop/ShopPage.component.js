e38016289d2a2a90059f45065f64bb4e409aa05f
Wellnaidy Adrian <wellnaidyadrian@Wellnaidys-MacBook-Air.local>
2021-06-30 22:09:06 +0700

    Updates shop page component state to use redux

--- src/pages/shop/ShopPage.component.js
+++ src/pages/shop/ShopPage.component.js
@@ -1,32 +1,22 @@
-import React, { Component } from 'react';
+import React from "react";
+import { connect} from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import './shoppage.styles.scss';
+import "./shoppage.styles.scss";
 
-import './shop.data.js';
-import SHOP_DATA from './shop.data.js';
+import CollectionPreview from "../collection-preview/Collection-preview.component";
+import { selectCollections } from "../../redux/shop/shop.selectors";
 
-import CollectionPreview from '../collection-preview/Collection-preview.component';
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...otherCollectionsProps }) => (
+      <CollectionPreview key={id} {...otherCollectionsProps} />
+    ))}
+  </div>
+);
 
-class ShopPage extends Component {
-    constructor(props){
-        super(props)
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    render(){
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionsProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionsProps} />
-                    ))
-                }
-            </div>
-        )
-    }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
