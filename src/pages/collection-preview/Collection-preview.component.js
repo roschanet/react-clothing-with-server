b475616597177f98360d1f2ddf813566a38062d7
Wellnaidy Adrian <wellnaidyadrian@Wellnaidys-MacBook-Air.local>
2021-07-01 11:26:02 +0700

    Updates title to upper case

--- src/pages/collection-preview/Collection-preview.component.js
+++ src/pages/collection-preview/Collection-preview.component.js
@@ -7,7 +7,7 @@ import CollectionItem from "../../components/collection-item/collection-item.com
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
           {items.filter((item, idx)=> idx < 4).map((item) => (
               <CollectionItem key={item.id} item={item} />
