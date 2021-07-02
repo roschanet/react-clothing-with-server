f132c584f9cb08c030f856f11a50a1478244b768
Wellnaidy Adrian <wellnaidyadrian@Wellnaidys-MacBook-Air.local>
2021-07-02 19:34:51 +0700

    Convert homepage, header, and custom button styles to use styled components

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { auth } from "../../firebase/firebase.utils";
 
-import { OptionDiv, OptionLink, HeaderContainer, LogoContainer, OptionsContainer } from "./header.styles";
+import { OptionLink, HeaderContainer, LogoContainer, OptionsContainer } from "./header.styles";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import CartIcon from "../cart-icon/cart-icon.component";
@@ -25,9 +24,9 @@ const Header = ({ currentUser, hidden }) => (
         KONTAK
       </OptionLink>
       {currentUser ? (
-        <OptionDiv onClick={() => auth.signOut()}>
+        <OptionLink as="div" onClick={() => auth.signOut()}>
           LOGOUT
-        </OptionDiv>
+        </OptionLink>
       ) : (
         <OptionLink to="/signin">
           REGISTER | LOGIN
